e8ea9b57688bcef19e2c84df12ac311f53aca444
Salih Sansarci <salihsansarci@gmail.com>
2025-05-10 21:09:38 +0300

    README.md hariç tüm dosyaları sildim

--- src/main/java/com/blog/blogapi/repository/TagRepository.java
+++
@@ -1,8 +0,0 @@
-package com.blog.blogapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.blog.blogapi.model.Tag;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
